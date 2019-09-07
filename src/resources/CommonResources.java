cbc78fa385c26dca856a32d82698bcf98b061a7b
Zhehao /Tony/ Lu <tonyzhehaolu@google.com>
2019-09-06 22:42:29 -0700

    Update server address

--- src/resources/CommonResources.java
+++ src/resources/CommonResources.java
@@ -1,7 +1,7 @@
 package resources;
 
 public interface CommonResources {
-	public static final String SQL_CONNECTION = "jdbc:mysql://206.189.208.57:3306/cs201_final_project_db?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+	public static final String SQL_CONNECTION = "jdbc:mysql://104.248.66.152:3306/cs201_final_project_db?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
 }
 
 
